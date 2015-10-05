98c9f8d252b37575221671edff67d92cecb3e1f3
unknown <html@2-01.itfactory.neta>
2015-10-05 19:11:34 +0200

    elso

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 // Fő angular module
 var cipobolt = angular.module("cipobolt",[]);
 
-cipobolt.controller("termekCtrl", function($scope){
+cipobolt.controller("termekCtrl", ["$http", "$scope", function($http, $scope){
     
     // Termékek listája
-    $scope.termekek = [
-        {"name": "Piros cipő", "price": "11000", "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"},
-        {"name": "Zöld cipő", "price": "11000", "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"},
-        {"name": "Kék cipő", "price": "11000", "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"},
-        {"name": "Fekete cipő", "price": "11000", "img": "http://keptaram.hu/kt/7/3451/rAPGoCGpduvPbCxVKPvn/piros-szaten-cipo.jpg"}
-    ];
-});
+    $scope.termekek = [];
+    
+    // Termékek lekérése
+    $http.get("js/json/termekek.json")
+    .success(function(d){
+        $scope.termekek = d;
+    })
+    .error(function(d){
+        console.error("Error: ", d);
+    });
+    
+    changeTheme();
+}]);
 
 
 
 // Téma módosítása
+
+function changeTheme(){
 $("#theme-selector a").click(function(e){
     
     // Esemény megálítása
@@ -35,3 +43,5 @@ $("#theme-selector a").click(function(e){
     
     
 });
+
+}
