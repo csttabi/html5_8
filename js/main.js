25a7ea9a24b268c0fc9a3d2847d7ae2f068eaf26
unknown <html@2-01.itfactory.neta>
2015-10-05 19:54:07 +0200

    vegso

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 // Fő angular module
-var cipobolt = angular.module("cipobolt",[]);
+var cipobolt = angular.module("cipobolt", []);
 
-cipobolt.controller("termekCtrl", ["$http", "$scope", function($http, $scope){
-    
+cipobolt.controller("termekCtrl", ["$http", "$scope", function ($http, $scope) {
+
     // Termékek listája
     $scope.termekek = [];
-    
+
     // Termékek lekérése
     $http.get("js/json/termekek.json")
-    .success(function(d){
-        $scope.termekek = d;
-    })
-    .error(function(d){
-        console.error("Error: ", d);
-    });
-    
+        .success(function (d) {
+            $scope.termekek = d;
+        })
+        .error(function (d) {
+            console.error("Error: ", d);
+        });
+
     changeTheme();
 }]);
 
@@ -22,26 +22,26 @@ cipobolt.controller("termekCtrl", ["$http", "$scope", function($http, $scope){
 
 // Téma módosítása
 
-function changeTheme(){
-$("#theme-selector a").click(function(e){
-    
-    // Esemény megálítása
-    e.preventDefault();
-    
-    // A kiválasztott link url-je.
-    var theme = this.href.replace(location.origin, "").replace(/\//g,"");
-    
-    
-    // Fő css link keresése és cseréje
-    //lib/bootswatch/cerulean/bootstrap.css
-    
-    var mainCss = $("#main-css");
-    var href = $("#main-css").attr("href");
-    href = href.replace(/bootswatch\/.*\/bootstrap/, "bootswatch/"+ theme +"/bootstrap");
-    
-    mainCss.attr("href",href);
-    
-    
-});
+function changeTheme() {
+    $("#theme-selector a").click(function (e) {
+
+        // Esemény megálítása
+        e.preventDefault();
+
+        // A kiválasztott link url-je.
+        var theme = this.href.replace(location.origin, "").replace(/\//g, "");
+
+
+        // Fő css link keresése és cseréje
+        //lib/bootswatch/cerulean/bootstrap.css
+
+        var mainCss = $("#main-css");
+        var href = $("#main-css").attr("href");
+        href = href.replace(/bootswatch\/.*\/bootstrap/, "bootswatch/" + theme + "/bootstrap");
+
+        mainCss.attr("href", href);
+
+
+    });
 
 }
